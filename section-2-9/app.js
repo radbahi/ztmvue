4b0bb6176a7f41ab654c9c0c573e1c24eb2699f7
Radouane Bahi <radbahi@gmail.com>
2022-03-18 09:18:11 -0400

    dynamic class binding and event modifiers

--- section-2-9/app.js
+++ section-2-9/app.js
@@ -1,20 +1,22 @@
 const vm = Vue.createApp({
+  //the data property is used to store single values, objects, and arrays. NOT functions.
   data() {
     return {
       firstName: "John",
+      middleName: '',
       lastName: "Doe",
       url: "https://google.com",
       age: 20,
     };
   },
-  //methods is where we define functions for our app
+  //methods is where we define functions for our app. we can make requests, update elements, etc.
   methods: {
     //arrow functions can mess up vue's proxy mechanisms, so stick to regular function for now
-    fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`;
-    },
     increment() {
       this.age++;
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value
     },
     updateLastName(msg, event) {
       //we can also use .prevent on the v-on directive in the html tag to do .preventDefault()
@@ -23,6 +25,21 @@ const vm = Vue.createApp({
       this.lastName = event.target.value;
     },
   },
+  //computed is where we would define functions that calculate a value. you must ALWAYS return a value. its more performant for calculations
+  //computed functions do NOT use parentheses when being invoked in expressions
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`;
+    },
+  },
+  //whenever a change occurs, you can run a function to execute additional logic, especially async tasks. not used often.
+  watch: {
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000)
+    }
+  }
 }).mount("#app");
 
 // setTimeout(() => {
