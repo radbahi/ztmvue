7dd7b5e0121f77ee96c80920f4460c7b60805b9c
Radouane Bahi <radbahi@gmail.com>
2022-04-05 13:27:03 -0400

    slot props refresh

---
+++ section-10-12-STARTER/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="app">
+		<!-- v-slot or # gives us access to the data in the HelloWorld component -->
+		<!-- we can also destruct which is how im pulling user -->
+		<hello-world #default="{user, favorites}">
+			<p>Hello {{ user.name }}. I like {{ favorites[0] }}.</p></hello-world
+		>
+	</div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+	name: "App",
+	components: {
+		HelloWorld,
+	},
+};
+</script>
---
+++ section-10-12-STARTER/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<!-- assigning user like this makes the data available to the parent scope -->
+		<slot :user="user" :favorites="favorites"></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "HelloWorld",
+	data() {
+		return {
+			user: {
+				name: "John",
+				age: 25,
+			},
+			favorites: ["Pizza", "Marbles", "Birds"],
+		};
+	},
+};
+</script>
